a5f6f3a843e01ec4ca87ca79a24a260e5edb9e9c
danilolimadev <danilolimadev@gmail.com>
2019-11-02 00:06:05 -0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),  // outside of the origin
+            padding: EdgeInsets.all(20.0),        // inside of the origin, in that case is Container
+            color: Colors.white,
+            child: Text('Hello'),                 // single child, can't have anymore in that widget
+          ),
+        ),
       ),
     );
   }
